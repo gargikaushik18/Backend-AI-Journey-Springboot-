f9ece3899e65952805ddd90406b627c9c4f4de1c
gargikaushik18 <gargikaushik1711@gmail.com>
2026-03-03 12:44:16 +0530

    Day 1:First Java Program

---
+++ Day1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your branch:");
+        String branch = sc.nextLine();
+
+        System.out.println("Hi " + name + ", welcome to Backend + AI journey ");
+        System.out.println("Your branch is: " + branch);
+
+        sc.close();
+    }
+}
