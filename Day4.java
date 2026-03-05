8f37f99536a60340e4ab3a55e6a9a317bb8e889d
gargikaushik18 <gargikaushik1711@gmail.com>
2026-03-06 02:38:10 +0530

    Day 4: Java loops practice

---
+++ Day4.java
@@ -0,0 +1,23 @@
+public class Day4 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        for (int i = 2; i <= 20; i = i + 2) {
+            System.out.println(i);
+        }
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+        int k = 1;
+
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 5);
+    }
+}
