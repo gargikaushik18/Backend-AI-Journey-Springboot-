2a5cf9166093b5f5725208b1b9af6ea4fe63ef90
gargikaushik18 <gargikaushik1711@gmail.com>
2026-03-06 20:44:59 +0530

    Arrays

---
+++ Day6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Day6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Numbers are:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+        int sum = 0;
+        System.out.println("Sum is :");
+        for (int i = 0; i < 5; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+
+        System.out.println("Largest Number is:");
+        int largi = arr[0];
+        for (int i = 1; i < 5; i++) {
+            if (arr[i] > largi) {
+                largi = arr[i];
+            }
+        }
+        System.out.println(largi);
+        sc.close();
+    }
+
+}
